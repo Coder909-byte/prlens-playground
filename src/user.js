4d24ad38fc4465542b1fe5bd1918d07e79e0ba8b
Jivesh Kumar Nazar <jivesh.nazar@gmail.com>
2026-09-18 12:32:33 +0530

    Modify getUserById to return email with user data

--- src/user.js
+++ src/user.js
@@ -1,5 +1,4 @@
 export function getUserById(users, id) {
-  const user = users.find((u) => u.id === id);
-  if (!user) return null;
-  return { id: user.id, name: user.name };
+  const user = users.find((u) => u.id == id);
+  return { id: user.id, name: user.name, email: user.email };
 }
